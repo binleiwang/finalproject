66ef1d00a4717b1b7b9537d04dddde10632a11c8
mynguyen94 <kieumynguyen94@gmail.com>
2017-06-19 03:49:56 -0700

    Update Robot.h

--- Robot.h
+++ Robot.h
@@ -10,21 +10,21 @@
 
 #include <string>
 #include <ostream>
+
 using namespace std;
 
 class Robot{
 
 private:
-	string name;
-	string asin; // Amazon Standard Identification Number
+	string name;	//primary key
+	string asin; 	//secondary key
 	double price;
 	string manufacturer;
 	string purpose;
 	string user;
 	double weight; // lbs
-	int rating; // average review rating 0-5.0
+	double rating; // average review rating 0-5.0
 	int qtyInStock;
-	string url;
 
 public:
 	//Constructor
@@ -38,27 +38,25 @@ public:
 	string get_purpose();
 	string get_user();
 	double get_weight();
-	int get_rating();
+	double get_rating();
 	int get_quantity();
-	string get_moreInfo();
 
 	/**Manipulation Procedures*/
-	void set_name(string a);
-	void set_asin(string b);
-	void set_price(double c);
-	void set_manufacture(string d);
-	void set_purpose(string e);
-	void set_user(string f);
-	void set_weight(double g);
-	void set_rating(int h);
-	void set_moreInfo(string i);
+	void set_name(string called);
+	void set_asin(string number);
+	void set_price(double cost);
+	void set_manufacture(string made);
+	void set_purpose(string use);
+	void set_user(string type);
+	void set_weight(double pounds);
+	void set_rating(int rate);
+	void set_quantity(int amount);
 
 	/**Additional Functions*/
 	friend ostream& operator<<(ostream& os, const Robot& robot);
 	bool operator==(const Robot& robot);
 	bool operator<(const Robot& robot);
 	bool operator>(const Robot& robot);
-
 };
 
 #endif /* ROBOT_H_ */

b1a6ec0c4215abbcdf0c0fddf076f481818b3a7c
kathynguyen1125 <thynguyen2511@gmail.com>
2017-06-25 15:48:47 -0700

    Update Order.cpp

--- Order.cpp
+++ Order.cpp
@@ -19,10 +19,13 @@ Order::Order() {
 	price = 0.0;
 	orderNum = 0;
 	priorityVal = 0;
-	opt = NULL;
+	opt = 'z';
 	orderTime = 0;
+	status = false;
+	totalPrice = 0.0;
 }
 /**Access Functions*/
+
 string Order::getName() {
 	return name;
 }
@@ -41,6 +44,35 @@ char Order::getOption() {
 time_t Order::getDate() {
 	return orderTime;
 }
+double Order::getTotal() {
+	return totalPrice;
+}
+bool Order::getStatus() {
+	return status;
+}
+double Order::shippingFee() {
+	if (getOption() == 'O' || getOption() == 'o')
+		return 10.99;
+	else if (getOption() == 'R' || getOption() == 'r') //option for 3 days shipping
+		return 6.99;
+	else
+		return 3.99;
+}
+string Order::shippingType() {
+	if (getOption() == 'O' || getOption() == 'o') {
+		//cout << "Your order will be delivered in 1 day.\n";
+		return "Overnight shipping";
+	}
+
+	else if (getOption() == 'R' || getOption() == 'r') { //option for 3 days shipping
+	//cout << "Your order will be delivered in 2-3 days.\n";
+		return "Rushing shipping";
+	} else {
+		//cout << "Your order will be delivered in 5-7 days.\n";
+		return "Standard shipping";
+	}
+
+}
 /**Manipulation Procedures*/
 void Order::setName(Robot R) {
 	name = R.get_name();
@@ -51,13 +83,13 @@ void Order::setPrice(Robot R) {
 void Order::setOrderNum(int num) {
 	orderNum = num;
 }
-void Order::setPriorityVal(int priority) {
-	if(opt == 'O'||opt =='o') //option for overnight
+void Order::setPriorityVal(char opt) {
+	if (opt == 'O' || opt == 'o') //option for overnight
 		priorityVal = 6;
-	else if(opt =='R'||opt =='r')//option for 3 days shipping
+	else if (opt == 'R' || opt == 'r') //option for 3 days shipping
 		priorityVal = 4;
 	else
-		priorityVal =1;
+		priorityVal = 1;
 }
 void Order::setOption(char o) {
 	opt = o;
@@ -65,23 +97,62 @@ void Order::setOption(char o) {
 void Order::setDate(time_t tm) {
 	orderTime = time(&tm); //function to display current time
 }
+void Order::setStatus(bool s) {
+	status = s;
+}
 void Order::addRobot(Robot R) {
 	cart.insertEnd(R);
 	price += R.get_price();
 }
-/**Additional Functions*/
+void Order::setTotal(List<Robot> cart) {
+	double tol = 0;
+	cart.beginIterator();
+	for (int i = 0; i < cart.getLength(); i++) {
+		tol += cart.getIterator().get_price();
+		cart.advanceIterator();
+	}
+	totalPrice = tol;
+}
+
+ostream& operator<<(ostream& os, const Order& O) {
+	os << "Order number: " << O.orderNum << endl;
+	for (int i = 0; i < O.cart.getLength(); i++) {
+		os << "Product " << i + 1 << ": " << O.name << endl;
+		os << "Price: $" << O.price << endl;
+		os << "Order time: " << O.orderTime << endl;
+	}
+	os << "Shipping option: " << O.shippingType() << "    Status:"
+			<< O.getStatus() << endl;
+	os << "Price before tax and handling: " << O.getTotal() << endl;
+	os << "Shipping fee: " << O.shippingFee() << endl;
+	os << "Total price: " << (O.getTotal() + O.shippingFee()) * 1.09;
+	return os;
+}
+bool Order::operator==(const Order& O) {
+	if (priorityVal != O.priorityVal)
+		return orderTime == O.orderTime;
+	else
+		return priorityVal == O.priorityVal;
+}
 
 bool Order::operator<(const Order& O) {
+	if (priorityVal == O.priorityVal)
+		return orderTime > O.orderTime;
+	else
+		return priorityVal > O.priorityVal;
+}
+
+bool Order::operator>(const Order& O) {
 	if (priorityVal == O.priorityVal)
 		return orderTime < O.orderTime;
 	else
 		return priorityVal < O.priorityVal;
 }
 
-bool Order::operator>(const Order& O) {
+bool Order::operator>=(const Order& O) {
 	if (priorityVal == O.priorityVal)
-		return orderTime > O.orderTime;
+		return orderTime <= O.orderTime;
 	else
-		return priorityVal > O.priorityVal;
+		return priorityVal <= O.priorityVal;
 }
 

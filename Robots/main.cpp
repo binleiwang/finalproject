174af2d0d80f10e54a1edc7bcc2900a5eae61405
stephenhung0408 <stephenhungog@gmail.com>
2017-06-27 10:49:09 -0700

    Update main.cpp

--- Robots/main.cpp
+++ Robots/main.cpp
@@ -76,8 +76,26 @@ bool isCustomer(string input)
 
 bool isEmployee(string input)
 {
+	string password;
+	int i = 3;
 	if (input == "E" || input == "e")
+	{
+		cout << "Please key in the password: ";
+		getline(cin, password);
+		while(password != "you suck")
+		{
+			i--;
+			if(i == 0)
+			{
+				cout <<  "System warning: You suck!" << endl;
+				return false;
+			}
+			cout << "Wrong password, you have " << i << " more chances" << endl;
+			cout << "Please key in the password: ";
+			getline(cin, password);
+		}
 		return true;
+	}
 	return false;
 }
 
